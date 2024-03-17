d24c5669c6b06c3f99e7a1208d845ea0d67db6f9
vyck22v7k527 <VyCK22V7K527@vlvh.ctu.edu.vn>
2024-03-17 09:13:09 +0700

    Tao trang them moi lien he

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -3,34 +3,34 @@
         <div class="form-group">
             <label for="name">Tên</label>
 
-            <Field name="name" type="text" class="form-control" v-, model="contactLocal.name" />
+            <Field name="name" type="text" class="form-control" v-model="contactLocal.name" />
 
             <ErrorMessage name="name" class="error-feedback" />
         </div>
         <div class="form-group">
             <label for="email">E-mail</label>
 
-            <Field name="email" type="email" class="form-control" v-, model="contactLocal.email" />
+            <Field name="email" type="email" class="form-control" v-model="contactLocal.email" />
 
             <ErrorMessage name="email" class="error-feedback" />
         </div>
         <div class="form-group">
             <label for="address">Địa chỉ</label>
 
-            <Field name="address" type="text" class="form-control" v-, model="contactLocal.address" />
+            <Field name="address" type="text" class="form-control" v-model="contactLocal.address" />
 
             <ErrorMessage name="address" class="error-feedback" />
         </div>
         <div class="form-group">
             <label for="phone">Điện thoại</label>
 
-            <Field name="phone" type="tel" class="form-control" v-, model="contactLocal.phone" />
+            <Field name="phone" type="tel" class="form-control" v-model="contactLocal.phone" />
 
             <ErrorMessage name="phone" class="error-feedback" />
         </div>
         <div class="form-group form-check">
 
-            <input name="favorite" type="checkbox" class="form-check-input" v-, model="contactLocal.favorite" />
+            <input name="favorite" type="checkbox" class="form-check-input" v-model="contactLocal.favorite" />
 
             <label for="favorite" class="form-check-label">
                 <strong>Liên hệ yêu thích</strong>
--- src/views/ContactEdit.vue
+++ src/views/ContactEdit.vue
@@ -27,6 +27,7 @@ export default {
                 this.contact = await ContactService.get(id);
             } catch (error) {
                 console.log(error);
+                console.log(error);
                 // Chuyển sang trang NotFound đồng thời giữ cho URL không đổi
                 this.$router.push({
                     name: "notfound",
